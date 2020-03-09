e3eaa20445362ce8223decbe62dce9ff849bdfca
bsouza97 <brunosouza97ti@gmail.com>
2020-03-08 22:02:47 -0300

    Fixing login

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,20 +1,11 @@
-// ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
 import './commands'
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+beforeEach('Login into system', () => {
+    const email = 'bruno.souza.ps1@townsq.com.br'
+    const password = 'bruno123'
+
+    cy.visit('/')
+    cy.get('[data-cy=email]').type(email)
+    cy.get('[data-cy=password]').type(password)
+    cy.get('[data-cy=submit]').click()
+})
